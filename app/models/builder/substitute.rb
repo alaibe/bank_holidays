8cbc4125b42fcb86e1a5641082fc611902f7ef2f
Anthony Laibe <anthony@laibe.cc>
2014-07-23 23:49:20 +0100

    Add rule for week end in us

--- app/models/builder/substitute.rb
+++ app/models/builder/substitute.rb
@@ -1,12 +1,12 @@
 class Builder::Substitute < Struct.new(:place)
 
   def get(method, date)
+    return date if [0, 6].include? date.wday
+
     send method, date
   end
 
   def next_available_weekday(date)
-    return date if [0, 6].include? date.wday
-
     new_date = if date.wday == 6
       date + 2.days
     elsif date.wday == 0
@@ -15,6 +15,14 @@ class Builder::Substitute < Struct.new(:place)
 
     new_date + 1.day while place.bank_holidays.find_by(on: new_date)
     new_date
+  end
+
+  def closest_weekday(date)
+    if date.wday == 6
+      date - 1.day
+    elsif date.wday == 0
+      date + 1.day
+    end
   end
 
 end
