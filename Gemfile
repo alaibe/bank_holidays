38b4ef68160f0bfa5ef56d14eef9f024f6fa7d9e
Anthony Laibe <anthony@laibe.cc>
2014-07-23 23:12:25 +0100

    Fix on filter by country

--- Gemfile
+++ Gemfile
@@ -36,9 +36,6 @@ gem 'spring',        group: :development
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-# Use debugger
-gem 'debugger', group: [:development, :test]
-
 gem 'friendly_id'
 gem 'slim'
 gem 'bootstrap-sass'
--- app/services/search.rb
+++ app/services/search.rb
@@ -6,13 +6,11 @@ class Search
     @date         = date
     @year         = year
     @country_isos = country_isos || []
-    @scopes       = BankHoliday
+    @scope        = BankHoliday
   end
 
   def all
-    @scopes.each do |scope|
-      by_date.by_year.by_country.scope
-    end
+    by_date.by_year.by_country.scope
   end
 
   def countries
@@ -24,14 +22,14 @@ class Search
   end
 
   def by_date
-    return self if !on.present?
+    return self unless on.present?
     @scope = @scope.where(on: on)
 
     self
   end
 
   def by_year
-    return self if on.present? || year.nil?
+    return self unless year.present?
     date = Date.new year.to_i
     @scope = @scope.where(on: date.beginning_of_year..date.end_of_year)
 
@@ -39,16 +37,17 @@ class Search
   end
 
   def by_country
-    return self if countries.empty
+    return self if countries.empty?
+
     ids = []
     if countries.any?
-      ids << @scope.where(place_id: countries, place_type: 'Country').pluck(:id)
+      ids += @scope.where(place_id: countries, place_type: 'Country').pluck(:id)
     end
 
     if states.any?
-      ids << = @scope.where(place_id: states, place_type: 'State').pluck(:id)
+      ids += @scope.where(place_id: states, place_type: 'State').pluck(:id)
     end
-    @scope += @scope.where(id: ids)
+    @scope = @scope.where(id: ids)
 
     self
   end
